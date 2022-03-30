526084f64f3db4d7fa3e8c0597a8b98fa9ce3246
noam ben baruch <noamfear369@gmail.com>
2022-03-30 21:16:39 +0300

    Re-examine the necessary number of labeled examples for successful training, numBatches = 1

--- main_lorenz.py
+++ main_lorenz.py
@@ -180,8 +180,8 @@ for rindex in range(0, len(r)):
    KNet_model.Build(sys_model)
    KNet_Pipeline.setModel(KNet_model)
    # KNet_Pipeline.setTrainingParams(n_Epochs=200, n_Batch=10, learningRate=1e-3, weightDecay=1e-4) # Original Settings
-   numEpochs = 100
-   numBatches = 1000
+   numEpochs = 1000
+   numBatches = 1
 
    logger.logEntry("Unsupervised Weight = " + str(unsupervised_weight))
    print(bcolors.UNDERLINE + bcolors.BOLD + "Unsupervised Weight = " + str(unsupervised_weight) + bcolors.ENDC)
@@ -192,7 +192,7 @@ for rindex in range(0, len(r)):
 
 # make a loop of size of data to train.
    # for num in range(10, train_input.size(0)+1, int(train_input.size(0) / 100)):
-   for num in range(25, 26, 1):
+   for num in range(1, 100, 10):
       num_labeled_examples = int(num)
       logger.set_num_labeled_examples(num_labeled_examples)
       num_test_examples = N_T       #int(num_examples / 5)
@@ -215,7 +215,7 @@ for rindex in range(0, len(r)):
                KNet_Pipeline.setTrainingParams(n_Epochs=numEpochs, n_Batch=numBatches, learningRate=1e-3, weightDecay=1e-4)
                # train_input2 = torch.index_select(train_input, 0, torch.tensor(range(num_examples)))
 
-      KNet_Pipeline.NNTrain(N_E, num_labeled_examples, train_input, train_target, num_cross_validation, cv_input, cv_target, unsupervised_weight, logger)
+      KNet_Pipeline.NNTrain(num_labeled_examples, num_labeled_examples, train_input, train_target, num_cross_validation, cv_input, cv_target, unsupervised_weight, logger)
 
       [KNet_MSE_test_linear_arr, KNet_MSE_test_linear_avg, KNet_MSE_test_dB_avg, KNet_test, KNet_test_obs] = KNet_Pipeline.NNTest(num_test_examples, test_input, test_target, unsupervised_weight, logger)
       KNet_Pipeline.save()
